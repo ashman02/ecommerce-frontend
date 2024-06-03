f048ee915ba69104da3651f76cb07e21ec4828fb
cforChobar <sidhuashman02@gmail.com>
2024-06-03 12:05:51 +0530

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
   const getCategories = async () => {
     setIsLoading(true)
     try {
-      const response = await axios.get("/api/v1/category/get-categories")
+      const response = await axios.get("https://chobarcart-api.onrender.com/api/v1/category/get-categories")
       setCategories(response.data.data)
       setIsLoading(false)
     } catch (error) {
