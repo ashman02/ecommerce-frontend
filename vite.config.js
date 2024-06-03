01f96d5e9c61a5c77294c8a8980f883ae688ba2a
cforChobar <sidhuashman02@gmail.com>
2024-06-03 12:49:47 +0530

    bug

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server : {
-    proxy : {
-      '/api/v1' : 'https://chobarcart-api.onrender.com/api/v1/'
-      // '/api/v1' : 'http://localhost:8000'
-    }
-  },
+  // server : {
+  //   proxy : {
+  //     '/api/v1' : 'https://chobarcart-api.onrender.com'
+  //     // '/api/v1' : 'http://localhost:8000'
+  //   }
+  // },
   plugins: [react()],
   resolve: {
     alias: {
