8284c87ce32ec081c7458ec473da1a0ef6fec896
cforChobar <sidhuashman02@gmail.com>
2024-06-04 11:38:57 +0530

    bug day 2

--- axiosConfig.js
+++ axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL : 'https://chobarcart-api.onrender.com/api/v1/',
+    // baseURL : 'https://chobarcart-api.onrender.com/api/v1/',
+    baseURL : 'http://localhost:8000/api/v1/',
     withCredentials : true
 })
 
