6874f6ab7921a626fe41058756fbce3291eb085a
cforChobar <sidhuashman02@gmail.com>
2024-09-25 19:44:52 +0530

    base url fix

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL : 'https://chobarcart-api.onrender.com',
+    baseURL : 'https://chobarcart-api.onrender.com/api/v1',
     withCredentials : true
 })
 
